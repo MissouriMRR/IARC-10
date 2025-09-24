339854d556da3743066cf8995815838fd1875c70
MattsAttack <wassermatthew1307@gmail.com>
2025-09-24 11:52:33 -0500

    Parse JSON to get ips and ports of other drones in client

--- Interdrone-Communication/client.py
+++ Interdrone-Communication/client.py
@@ -12,6 +12,26 @@ class Client:
         self.jsonData = jsonData
         self.droneId: str = jsonData["localInfo"]["selfId"]
 
+        # Parse JSON to get IPs and Ports of drones to connect to
+
+        # Instantiate otherDrones lists
+        self.otherDronesIps: list[str]
+        self.otherDronesPorts: list[int]
+
+        # Loop through drones 1-4
+        for i in range(1, 5):
+            # If drone is self (drone running this script) don't add them otherDrones list
+            if i != int(self.droneId):
+                # Add other drones IP and Ports to their respective lists
+                self.otherDronesIps.append(
+                    self.jsonData["drones"][str(i)]["ip"]
+                )  # This will be simplified after JSON parser is implemented
+                self.otherDronesPorts.append(
+                    int(self.jsonData["drones"][str(i)]["port"])
+                )
+        print(self.otherDronesIps)
+        print(self.otherDronesPorts)
+
     # Start client and try to send messages to servers
     # TODO. Once drone list is implemented, split into setup and runtime functions
     def start_client(self):
