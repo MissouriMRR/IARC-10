558a247175a551e7ea4c47a9bd9604d862699296
Jack Dabrowski <JacksonGDabrowski@gmail.com>
2025-10-28 20:49:32 -0500

    Added Field class to hold mine and node positions.

--- pathfinding/rexAlg/nodeGen.py
+++ pathfinding/rexAlg/nodeGen.py
@@ -1,69 +1,170 @@
 import matplotlib.pyplot as pyplot
 import matplotlib.collections as mc
 import numpy as np
+import random
 """
-Each mine requires at least 4 nodes that connect to another mine's other 4 nodes.
+Use the Field class to generate mines and their nodes.
 Ex:
-    mine1 = Mine(centerX,centerY,radius)
-    mine2 = Mine(centerX,centerY,radius)
+    field = Field(-100,150,-100,150,3,16)
 
-    Node Setup:
-        Node(parent,target,internal(T/F),primary(T/F))
+    Field(xMin,xMax,yMin,yMax,numMines,mineRadius)
+    Field Setup:
+     - xMin,xMax : The fields x axis range
+     - yMin, yMax : The fields y axis range
+     - numMines : Number of mines to be generated for Node testing
+     - mineRadius : The mines' radii
 
-    internal/external Bitangents:
-      - internal:True
-      - external:False
-    Primary/Secondary Points
-      - Primary:True
-      - Secondary:False
-
-    Repeat for each mine:
-        Rememebr to swap parent and target depending on how 
-        you want to connect the mines
-
-        mine1InterNode1 = Node(mine1,mine2,True,True)
-        mine1InterNode2 = Node(mine1,mine2,True,False)
-        mine1ExterNode1 = Node(mine1,mine2,False,True)
-        mine1ExterNode2 = Node(mine1,mine2,False,False)
 """
+
+# Field generates nodes off of mines, temporarily generates mines too
+class Field:
+    def __init__(self,xMin,xMax,yMin,yMax,numMines,mineRadius):
+        self.mines:Mine = []
+        self.xMin = xMin
+        self.yMin = yMin
+        self.xMax = xMax
+        self.yMax = yMax                            
+    
+    def addMine(self,centerX,centerY,radius):
+        self.mines.append(Mine(centerX,centerY,radius))
+        for num,mine in enumerate(self.mines):
+            # Nodes get created only after the 1st iteration
+            if num > 0:
+                currentMine = self.mines[num]
+                prevMine = self.mines[num-1]
+                # Previous Mine's Node Creation
+                prevMine.addNode(Node(prevMine,currentMine,True,True)) # Previous mine's primary internal node created
+                prevMine.addNode(Node(prevMine,currentMine,True,False))
+                prevMine.addNode(Node(prevMine,currentMine,False,True)) # Previous mine's primary external node created
+                prevMine.addNode(Node(prevMine,currentMine,False,False))
+
+                # Current Mine's Node Creation
+                currentMine.addNode(Node(currentMine,prevMine,True,True))
+                currentMine.addNode(Node(currentMine,prevMine,True,False))
+                currentMine.addNode(Node(currentMine,prevMine,False,True))
+                currentMine.addNode(Node(currentMine,prevMine,False,False))
+
+                # Gather each mines' nodes to prepare for connecting
+                prevMineNodes = prevMine.getNodes()
+                currentMineNodes = currentMine.getNodes()
+                for prevMineNode in prevMineNodes:
+                    for currentMineNode in currentMineNodes:
+                        # Internal connections(Previous Mine's internal primary to Current Mine's internal secondary)
+                        if (prevMineNode.internal and prevMineNode.primary) and (currentMineNode.internal and not currentMineNode.primary):
+                            prevMineNode.connectNode(currentMineNode)
+                        elif(prevMineNode.internal and not prevMineNode.primary) and (currentMineNode.internal and currentMineNode.primary):
+                            prevMineNode.connectNode(currentMineNode)
+                        # External connections(Previous Mine's external primary to Current Mine's external primary)
+                        elif (not prevMineNode.internal and prevMineNode.primary) and (not currentMineNode.internal and currentMineNode.primary):
+                            prevMineNode.connectNode(currentMineNode)
+                        elif (not prevMineNode.internal and not prevMineNode.primary) and (not currentMineNode.internal and not currentMineNode.primary):
+                            prevMineNode.connectNode(currentMineNode)
+                
+                # TODO: Node Creation of earlier mines(Connect ALL the mines)
+
+    # Plotting using matplotlib
+    def plotField(self):
+        plt = pyplot
+        mineColors = ['red','blue','orange','magenta','cyan']
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+        plt.xlim(self.xMin,self.xMax)
+        plt.ylim(self.yMin,self.yMax)
+        
+        # Create a list of circles representing mines, centered to their correlated mine's center
+        circles = [plt.Circle(self.mines[i].getPos(),self.mines[i].getRadius(),color=np.random.choice(mineColors)) for i in range(len(self.mines))]
+        
+        # Plot the mines
+        for circle in circles:
+            ax.add_patch(circle)
+
+        # Plot the nodes
+        allNodes = Node.nodes
+        for node in allNodes:
+            # NOTE: May create duplicate lines due to nodes connecting to each other twice
+            plt.plot([node.x,node.connectedNode.x],[node.y,node.connectedNode.y],'o',color='black')
+        
+        plt.show()
 
 """MATH STUFF"""
-# Mine class keeps track of mine position and radii
+# Mine class keeps track of mine position and radii      
 class Mine:
     numMines = 0
-    def __init__(self,centerX,centerY,radius):
-        self.x, self.y, self.radius = float(centerX) , float(centerY), float(radius)
+    mines = []
+    def __init__(self,centerX,centerY,radius,name:str=''):
         Mine.numMines += 1
-        self.nodes = []
+        if len(name) > 0:
+            self.name = name
+        else:
+            self.name = f'Mine ID: ' + str(self.numMines)
+        self.x, self.y, self.radius = np.round(centerX,2) , np.round(centerY,2), np.round(radius,2)
+        # Node storage
+        self.__nodes = []
+        self.__primary = []
+        self.__secondary = []
+        self.__internal = []
+        self.__external = []
+        Mine.mines.append(self)
+        
         
     def getPos(self):
         return self.x,self.y
     def getRadius(self):
         return self.radius
-    def getNodes(self):
-        return self.nodes
+    def getNodes(self,type:str=''):
+        if type == None or type == '':
+            return self.__nodes
+        elif type.lower() == 'primary':
+            return self.__primary
+        elif type.lower() == 'secondary':
+            return self.__secondary
+        elif type.lower() == 'internal':
+            return self.__internal
+        elif type.lower() == "external":
+            return self.__external
+        else:
+            return 'invalid node type'
     def addNode(self,node:"Node"):
-        self.nodes.append(node)
+        self.__nodes.append(node)
+        internal = node.internal
+        primary = node.primary
+        if internal:
+            self.__internal.append(node)
+        else:
+            self.__external.append(node)
+        
+        if primary:
+            self.__primary.append(node)
+        else:
+            self.__secondary.append(node)
+
     def __str__(self):
-        return f'pos: {self.x,self.y} radius:{self.radius}'
+        return self.name
     def __repr__(self):
         return self.__str__()
 
 # Node class keeps track of node positions
 class Node:
-
-    def __init__(self,parentMine:"Mine",targetMine:"Mine",internal:bool=True,primary:bool=True):
+    nodeNum = 0
+    nodes = []
+    def __init__(self,parentMine:"Mine",targetMine:"Mine",internal:bool=True,primary:bool=True,name:str=''):
+        Node.nodeNum += 1
+        if len(name) < 1:
+            self.name = "Node ID: "+ str(Node.nodeNum)
+        else:
+            self.name = name 
         self.parentMine = parentMine
-        self.connectedNodes = []
+        self.connectedNode = None
         self.x = 0.0
         self.y = 0.0
+        self.connected = False
         self.__targetMine = targetMine
-        parentMine.addNode(self)
+
         d = np.sqrt((parentMine.x-targetMine.x)**2+(parentMine.y-targetMine.y)**2) # Algabraic Distance Formula
 
+        self.internal = internal
         self.primary= primary # Primary node is the first node where it is placed 
                               # (typically towards the top of the circle)
-
         # Create Angle Offset(relative to target mine). It changes slightly depending on mines' positions
         # Formula is: arccos(x1-x2)/d+pi
         if parentMine.y > targetMine.y:
@@ -79,7 +180,7 @@ class Node:
         # Offset Angle is the same for internal and external bitangents
         if internal:
             # Create internal angle
-            internalAngle = np.arccos(((parentMine.radius)+(targetMine.radius))/d)
+            internalAngle = np.arccos(np.clip(((parentMine.radius)+(targetMine.radius))/d,-1,1))
             
             if primary:
                 self.x = ((parentMine.radius) * np.cos(internalAngle+offsetAngle)) + parentMine.x
@@ -97,7 +198,8 @@ class Node:
             else:
                 self.x = (parentMine.radius) * np.cos(externalAngle-(offsetAngle)) + parentMine.x
                 self.y = (parentMine.radius) * np.sin(externalAngle-(offsetAngle)+np.pi) + parentMine.y
-
+        Node.nodes.append(self)
+        
     def distanceFromNode(self,node:"Node"):
         distance = 0
         if len(self.connectedNodes) > 0 and node in self.connectedNodes:
@@ -105,8 +207,10 @@ class Node:
         return distance
 
     def connectNode(self,node:"Node"):
-        self.connectedNodes(node)
-        node.connectedNodes(self)
+        self.connectedNode = node
+        node.connectedNode = self
+        self.connected = True
+        node.connected = True
         return node
 
     def get_pos(self):
@@ -115,109 +219,27 @@ class Node:
     def getTargetMine(self):
         return self.__targetMine
     def __str__(self):
-        return f"Parent Mine: {self.parentMine}"
+        return self.name
     def __repr__(self):
         return self.__str__()
 
+numMines = 3
+xMin = -100
+xMax = 150
+yMin = -100
+yMax = 150
+radius = 16
+position = [0,0]
+field = Field(xMin,xMax,yMin,yMax,numMines,radius)
 
-sample = Mine(25,25,16)
-otherSample = Mine(0,100,16)
-mine = Mine(100,75,16)
-
-"""""""""Internal Bitangents"""""""""
-# sample and other sample connections
-sampleNode1 = Node(sample,otherSample,True)
-sampleNode2 = Node(sample,otherSample,True,False)
-
-otherSampleNode1 = Node(otherSample,sample,True)
-otherSampleNode2 = Node(otherSample,sample,True,False)
-
-# mine and sample connections
-mineToSampleInterNode1 = Node(mine,sample,True)
-mineToSampleInterNode2 = Node(mine,sample,True,False)
-
-sampleToMineInterNode1 = Node(sample,mine,True)
-sampleToMineInterNode2 = Node(sample,mine,True,False)
-
-# mine and other sample connections
-mineToOtherSampleInterNode1 = Node(mine,otherSample,True)
-mineToOtherSampleInterNode2 = Node(mine,otherSample,True,False)
-
-otherSampleToMineInterNode1 = Node(otherSample,mine,True)
-otherSampleToMineInterNode2 = Node(otherSample,mine,True,False)
-
-"""""""""External Bitangents"""""""""
-# sample and other sample connections
-sampleExternalNode1 = Node(sample,otherSample,False)
-sampleExternalNode2 = Node(sample,otherSample,False,False)
-
-sampleExternalNode3 = Node(otherSample,sample,False)
-sampleExternalNode4 = Node(otherSample,sample,False,False)
-
-# mine and sample connections
-mineToSampleExternNode1 = Node(mine,sample,False)
-mineToSampleExternNode2 = Node(mine,sample,False,False)
-
-sampleToMineExternNode1 = Node(sample,mine,False)
-sampleToMineExternNode2 = Node(sample,mine,False,False)
-
-# mine and other sample connections
-mineToOtherSampleExternNode1 = Node(mine,otherSample,False)
-mineToOtherSampleExternNode2 = Node(mine,otherSample,False,False)
-
-otherSampleToMineExternNode1 = Node(otherSample,mine,False)
-otherSampleToMineExternNode2 = Node(otherSample,mine,False,False)
-
-# # Connect the Nodes
-# sampleNode1.connectNode(otherSampleNode1)
-# sampleNode2.connectNode(otherSampleNode2)
-
-# sampleExternalNode1.connectNode(sampleExternalNode3)
-# sampleExternalNode2.connectNode(sampleExternalNode4)
-""" MATPLOTLIB STUFF"""
-plt = pyplot
-
-# Circles/Mines
-sampleCircle = pyplot.Circle(sample.getPos(),sample.getRadius(),color='r')
-otherSampleCircle = pyplot.Circle(otherSample.getPos(),otherSample.getRadius(),color='b')
-mineCircle = pyplot.Circle(mine.getPos(),mine.getRadius(),color="black")
-
-fig, ax = pyplot.subplots()
-ax.set_aspect("equal")
-ax.add_patch(sampleCircle)
-ax.add_patch(otherSampleCircle)
-ax.add_patch(mineCircle)
-
-"""""""""Plot Internal Bitangents"""""""""
-# sample and other sample internal connect
-plt.plot([sampleNode1.x,otherSampleNode1.x],[sampleNode1.y,otherSampleNode1.y],color="y")
-plt.plot([otherSampleNode2.x,sampleNode2.x],[otherSampleNode2.y,sampleNode2.y],color='g')
-
-# mine and sample internal connect
-plt.plot([mineToSampleInterNode1.x,sampleToMineInterNode1.x],[mineToSampleInterNode1.y,sampleToMineInterNode1.y],color='y')
-plt.plot([mineToSampleInterNode2.x,sampleToMineInterNode2.x],[mineToSampleInterNode2.y,sampleToMineInterNode2.y],color='g')
-
-# mine and other sample internal connect
-plt.plot([mineToOtherSampleInterNode1.x,otherSampleToMineInterNode1.x],[mineToOtherSampleInterNode1.y,otherSampleToMineInterNode1.y],color='y')
-plt.plot([mineToOtherSampleInterNode2.x,otherSampleToMineInterNode2.x],[mineToOtherSampleInterNode2.y,otherSampleToMineInterNode2.y],color='g')
-
-"""""""""Plot External Bitangents"""""""""
-# sample and other sample external connect
-plt.plot([sampleExternalNode1.x,sampleExternalNode4.x],[sampleExternalNode1.y,sampleExternalNode4.y],color='c')
-plt.plot([sampleExternalNode2.x,sampleExternalNode3.x],[sampleExternalNode2.y,sampleExternalNode3.y],color='m')
-
-""" 
-#For whatever reason, a bug where one of the external nodes is displayed slightly off.
-#But, the node position itself through printing to console appears to be correctly calculated.
-"""
-# mine and sample external connect
-plt.plot([mineToSampleExternNode1.x,sampleToMineExternNode2.x],[mineToSampleExternNode1.y,sampleToMineExternNode2.y],color='c')
-plt.plot([mineToSampleExternNode2.x,sampleToMineExternNode1.x],[mineToSampleExternNode2.y,sampleToMineInterNode1.y],color='m')
-
-# mine and other sample external connect
-plt.plot([mineToOtherSampleExternNode1.x,otherSampleToMineExternNode2.x],[mineToOtherSampleExternNode1.y,otherSampleToMineExternNode2.y],color='c')
-plt.plot([mineToOtherSampleExternNode2.x,otherSampleToMineExternNode1.x],[mineToOtherSampleExternNode2.y,otherSampleToMineExternNode1.y],color='m')
-
-plt.xlim(-100,150)
-plt.ylim(-100,150)
-plt.show()
+# Mine generation
+for num in range(numMines):
+    while True: # To make sure generated mines arent clipping off the edges of the field
+        #           TODO: prevent mines from overlapping
+        position[0], position[1] = random.randint(xMin,xMax+1),random.randint(yMin,yMax+1)
+        if position[0] <= xMin + radius or position[0] >= xMax - radius or position[1] <= yMin + radius or position[1] >= yMax - radius:
+            continue
+        
+        break
+    field.addMine(position[0],position[1],radius)
+field.plotField()
