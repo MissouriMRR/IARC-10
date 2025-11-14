b14f6b42e328ff35154b43e2b7de3e6ad2c6a3b5
Rex Berry <rex.berry@mst.edu>
2025-11-13 20:12:28 -0600

    Update functions for mine detection

--- vision/mine_detection/detect_mines.py
+++ vision/mine_detection/detect_mines.py
@@ -8,9 +8,9 @@ from vision.common import Attitude, BoundingBox, Coordinate
 
 def detect_mines(
     image: cv2.typing.MatLike, coord: Coordinate, attitude: Attitude
-) -> Iterable[BoundingBox]:
+) -> Iterable[tuple[Coordinate, float]]:
     """
-    Detect mines in the given image.
+    Detect mines in the given image and return coordinates.
 
     Parameters
     ----------
@@ -23,7 +23,24 @@ def detect_mines(
 
     Returns
     -------
+    Iterable[tuple[Coordinate, float]]
+        The coordinates and confidence scores of the detected mines.
+    """
+    raise NotImplementedError("Mine detection not yet implemented.")
+
+
+def detect_mines_in_image(image: cv2.typing.MatLike) -> Iterable[BoundingBox]:
+    """
+    Detect mines in the given image and return bounding boxes.
+
+    Parameters
+    ----------
+    image : cv2.typing.MatLike
+        The input image in which to detect mines.
+
+    Returns
+    -------
     Iterable[BoundingBox]
-        An iterable of BoundingBox instances representing detected mines.
+        The bounding boxes of the detected mines.
     """
     raise NotImplementedError("Mine detection not yet implemented.")
