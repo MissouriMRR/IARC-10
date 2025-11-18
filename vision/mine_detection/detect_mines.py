428bb2bcaf7fa627277eee7cb93518c695ea916e
Rex Berry <rex.berry@mst.edu>
2025-11-17 18:18:12 -0600

    Add CameraInfo class

--- vision/mine_detection/detect_mines.py
+++ vision/mine_detection/detect_mines.py
@@ -3,11 +3,14 @@ from collections.abc import Iterable
 import cv2
 import cv2.typing
 
-from vision.common import Attitude, BoundingBox, Coordinate
+from vision.common import Attitude, BoundingBox, CameraInfo, Coordinate
 
 
 def detect_mines(
-    image: cv2.typing.MatLike, coord: Coordinate, attitude: Attitude
+    image: cv2.typing.MatLike,
+    coord: Coordinate,
+    attitude: Attitude,
+    camera_info: CameraInfo,
 ) -> Iterable[tuple[Coordinate, float]]:
     """
     Detect mines in the given image and return coordinates.
@@ -20,6 +23,8 @@ def detect_mines(
         The GPS coordinate of the drone and camera when the image was captured.
     attitude : Attitude
         The combined attitude of the drone and camera when the image was captured.
+    camera_info : CameraInfo
+        The camera information including field of view.
 
     Returns
     -------
