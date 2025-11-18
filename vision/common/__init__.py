a82cbe51f860f0f2845aa3585f8c38e4f82be01d
Rex Berry <rex.berry@mst.edu>
2025-11-17 18:18:58 -0600

    Add Coordinate.from_image_coord() method declaration

--- vision/common/__init__.py
+++ vision/common/__init__.py
@@ -1,3 +1,4 @@
 from vision.common.attitude import Attitude
 from vision.common.bounding_box import BoundingBox
+from vision.common.camera_info import CameraInfo
 from vision.common.coordinate import Coordinate
--- vision/common/coordinate.py
+++ vision/common/coordinate.py
@@ -2,6 +2,11 @@
 
 from dataclasses import dataclass
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from vision.common.attitude import Attitude
+    from vision.common.camera_info import CameraInfo
+
 
 @dataclass(frozen=True)
 class Coordinate:
@@ -24,3 +29,40 @@ class Coordinate:
     lat: float
     lon: float
     alt: float
+
+    @staticmethod
+    def from_image_coord(
+        x: float,
+        y: float,
+        image_width: float,
+        image_height: float,
+        camera_coord: "Coordinate",
+        attitude: "Attitude",
+        camera_info: "CameraInfo",
+    ) -> "Coordinate":
+        """
+        Convert image coordinates to a geographic coordinate.
+
+        Parameters
+        ----------
+        x : float
+            The x-coordinate in the image.
+        y : float
+            The y-coordinate in the image.
+        image_width : float
+            The width of the image.
+        image_height : float
+            The height of the image.
+        coord : Coordinate
+            The GPS coordinate of the drone and camera when the image was captured.
+        attitude : Attitude
+            The combined attitude of the drone and camera when the image was captured.
+        camera_info : CameraInfo
+            The camera information including field of view.
+
+        Returns
+        -------
+        Coordinate
+            The geographic coordinate corresponding to the image coordinates.
+        """
+        raise NotImplementedError("Coordinate conversion not yet implemented.")
