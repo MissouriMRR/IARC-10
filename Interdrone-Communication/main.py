bbc219ede8e33624bb2a0dc011a0af9c7b95632e
MattsAttack <wassermatthew1307@gmail.com>
2026-02-01 17:14:52 -0600

    remove deprecated startup script code

--- Interdrone-Communication/main.py
+++ Interdrone-Communication/main.py
@@ -16,7 +16,6 @@ def main() -> None:
     # Parse arguments in main thread
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--id", help="Self ID", type=int)
-    # parser.add_argument("-s", "--skip", help="Startup override (1=true)", type=int)
     args = parser.parse_args()
 
     # Load config
