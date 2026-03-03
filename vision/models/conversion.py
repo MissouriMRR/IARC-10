a137623177bf5f0cae13a97870bfe858575b9849
Owen Estes <oje3c2@umsystem.edu>
2026-03-02 18:54:36 -0600

    added the files to convert to imx model

--- vision/models/conversion.py
+++ vision/models/conversion.py
@@ -1,6 +1,19 @@
+'''
+# the simple ultralytics way to convert to rpk(literally doesn't work)
 import torch
 from ultralytics import YOLO
-
-# The simple Ultralytics way - just do this:
 model = YOLO('2-16-2026/v11s_2-16.pt')
-model.export(format='imx')
+model.export(format='imx')'''
+
+'''
+# the simple ultralytics way to convert to onnx(literally doesn't work)
+import torch
+from ultralytics import YOLO
+model = YOLO('2-16-2026/v11s_2-16.pt')
+model.export(format='onnx')'''
+
+# the other way to convert to .rpk
+import model_compression_toolkit as mct
+import imx500_converter
+
+
