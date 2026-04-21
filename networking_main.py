ffebce7daf582105742e1d0f7b932049f1a13d33
MattsAttack <wassermatthew1307@gmail.com>
2026-04-20 21:13:09 -0500

    Create separate networking main file #45

---
+++ networking_main.py
@@ -0,0 +1,28 @@
+from state_machine.drone import Drone
+from state_machine.flight_settings import FlightSettings
+from state_machine.interdrone import Interdrone
+import asyncio
+import argparse
+
+
+async def main():
+    # Get drone ID arg
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--id", help="Self ID", type=int)
+    args = parser.parse_args()
+
+    drone: Drone = Drone()
+    flight_settings: FlightSettings = FlightSettings.from_mission_config(self_id=args.id)
+    interdrone: Interdrone = Interdrone(flight_settings=flight_settings, drone=drone)
+    interdroneTask = asyncio.create_task(interdrone.start_interdrone())
+    try:
+        # Keep the networking loop alive
+        while True:
+            await asyncio.sleep(1)
+    except asyncio.CancelledError:
+        print("Networking shutting down...")
+        interdroneTask.cancel()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
